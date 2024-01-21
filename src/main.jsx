04240b03eff985f0795322691637dcdd41d2a30e
Were Samson Bruno <werefast2000@gmail.com>
2024-01-21 23:53:49 +0000

    Import App from app.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { createRoot } from "react-dom/client";
-import TontinePageLayout from "./components/TontinePageLayout";
+import App from "./app";
 import { mockData } from "./mockData";
 import "./styles/reset.css";
 import "./styles/variables.css";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+
+
 root.render(
   <div >
-    <TontinePageLayout {...mockData} />
+    <App {...mockData} />
   </div>
 );
