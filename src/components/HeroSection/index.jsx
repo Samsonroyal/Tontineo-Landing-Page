82a1bf125be3e83f50359d5807595543ff7bd670
Were Samson Bruno <werefast2000@gmail.com>
2024-01-21 11:59:15 +0000

    created hero section component

---
+++ src/components/HeroSection/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const HeroSection = () => {
+    return (
+        <div className="container-wrapper">
+            <div className="container-with-images-and-button">
+                <img src="/assets/img_1_118_3310b9.svg" className="image-container15" />
+                <button className="base-Button-root button-container" type="button">
+                    <div className="svg-container1 svg-container1">
+                        <svg
+                            fill="none"
+                            viewBox="0 0 32 32"
+                            xmlns="http://www.w3.org/2000/svg"
+                            x="0"
+                            y="0"
+                        >
+                            <path
+                                id="Vector"
+                                d="M15.7865,21.057c0.025,0.032 0.058,0.058 0.095,0.076c0.037,0.018 0.077,0.028 0.119,0.028c0.041,0 0.082,-0.01 0.119,-0.028c0.037,-0.018 0.069,-0.044 0.095,-0.076l3.801,-4.809c0.139,-0.177 0.013,-0.438 -0.214,-0.438h-2.515v-11.485c0,-0.149 -0.122,-0.271 -0.272,-0.271h-2.036c-0.149,0 -0.271,0.122 -0.271,0.271v11.481h-2.508c-0.228,0 -0.353,0.262 -0.214,0.438zM28.4215,19.869h-2.036c-0.15,0 -0.272,0.122 -0.272,0.271v5.227h-20.227v-5.227c0,-0.149 -0.122,-0.271 -0.271,-0.271h-2.037c-0.149,0 -0.271,0.122 -0.271,0.271v6.72c0,0.601 0.485,1.086 1.086,1.086h23.213c0.601,0 1.086,-0.485 1.086,-1.086v-6.72c0,-0.149 -0.122,-0.271 -0.271,-0.271z"
+                                fill="currentColor"
+                                xmlns="http://www.w3.org/2000/svg"
+                                data-node-id="1:123"
+                            ></path>
+                        </svg>
+                    </div>
+                    Download the App
+                </button>
+                <div className="image-container1">
+                    <img src="/assets/img_1_483_2c6b1e.png" className="image-container14" />
+                </div>
+                <img src="/assets/img_1_480_5347ca.svg" className="image-container9" />
+                <img src="/assets/img_1_479_2365db.svg" className="image-container2" />
+                <div className="image-container7">
+                    <img src="/assets/img_1_481_c52e51.svg" className="image-container13" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HeroSection;
