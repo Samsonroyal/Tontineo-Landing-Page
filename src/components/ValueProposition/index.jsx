08ea8e95a5e9dc78f88865a5c0fa2bf37484951e
Were Samson Bruno <werefast2000@gmail.com>
2024-01-21 23:51:23 +0000

    created value proposition component

---
+++ src/components/ValueProposition/index.jsx
@@ -0,0 +1,214 @@
+import React from "react";
+
+const ValueProposition = () => {
+  return (
+    <div >
+      <div>
+        <div >
+          <div >
+            <div class="app-value-section">
+              <p class="app-value">The value you get for using our app</p>
+              <div class="tontine-container2">
+                <div class="tontine-container1">
+                  <div class="svg-container3">
+                    <svg
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    ></svg>
+                  </div>
+                  <p
+                    class="tontine-value-text"
+                    style="text-align: left; flex-shrink: 1;"
+                  >
+                    Save more on your daily and weekly tontine expenses
+                  </p>
+                </div>
+                <div class="tontine-container1">
+                  <div class="svg-container3">
+                    <svg
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    ></svg>
+                  </div>
+                  <p
+                    class="tontine-value-text"
+                    style="text-align: left; flex-shrink: 1;"
+                  >
+                    Achieve the goals that matter to you faster.
+                  </p>
+                </div>
+                <div class="tontine-container1">
+                  <div class="svg-container3">
+                    <svg
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    ></svg>
+                  </div>
+                  <p class="tontine-value-text">Access more capital</p>
+                </div>
+                <div class="tontine-container1">
+                  <div class="svg-container3">
+                    <svg
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    ></svg>
+                  </div>
+                  <p class="tontine-value-text">
+                    Your contributions are secured
+                  </p>
+                </div>
+              </div>
+              <div class="app-benefit">
+                <p class="learn-more1">Learn More</p>
+                <div class="svg-container2">
+                  <svg
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  ></svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="app-benefit">
+          <p className="learn-more1">Learn More</p>
+          <div className="svg-container2 svg-container2">
+            <svg
+              fill="none"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              x="0"
+              y="0"
+            >
+              <g
+                id="arrow-right_3"
+                xmlns="http://www.w3.org/2000/svg"
+                data-node-id="1:595"
+              >
+                <path
+                  id="Vector_72"
+                  d="M5,12h14"
+                  stroke="white"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  data-node-id="1:596"
+                ></path>
+                <path
+                  id="Vector_73"
+                  d="M12,5l7,7l-7,7"
+                  stroke="white"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  data-node-id="1:597"
+                ></path>
+              </g>
+            </svg>
+          </div>
+        </div>
+      </div>
+
+      <div className="savings-summary">
+        <div className="savings-card1">
+          <div className="green-rounded-card">
+            <img
+              src="/assets/img_1_600_bb5850.png"
+              className="image-container3"
+            />
+          </div>
+          <div className="savings-card"></div>
+          <div className="savings-card2">
+            <div className="tontine-container1">
+              <div className="wardrobe-saving2">
+                <div className="svg-container3 svg-container3">
+                  <svg
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    x="0"
+                    y="0"
+                  >
+                    <g
+                      id="Import"
+                      xmlns="http://www.w3.org/2000/svg"
+                      data-node-id="1:606"
+                    >
+                      <path
+                        id="Vector 9"
+                        d="M7,9l5,5M12,14l5,-5M12,14v-9"
+                        stroke="#403BD7"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        data-node-id="I1:606;281:2194"
+                      ></path>
+                      <path
+                        id="Vector 114"
+                        d="M5,16v1c0,1.1 0.895,2 2,2h10c1.105,0 2,-0.9 2,-2v-1"
+                        stroke="#403BD7"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        data-node-id="I1:606;281:2195"
+                      ></path>
+                    </g>
+                  </svg>
+                </div>
+              </div>
+              <div className="wardrobe-saving">
+                <p className="wardrobe-title">Wardrobe</p>
+                <p className="wardrobe-saving1">Saving</p>
+              </div>
+            </div>
+            <p className="price-label">$40.00</p>
+          </div>
+          <div className="savings-container"></div>
+          <div className="savings-container2">
+            <div className="savings-container1">
+              <div className="svg-container4 svg-container4">
+                <svg
+                  fill="none"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0"
+                  y="0"
+                >
+                  <g
+                    id="Frame 8749"
+                    xmlns="http://www.w3.org/2000/svg"
+                    data-node-id="1:615"
+                  >
+                    <rect
+                      height="28"
+                      width="28"
+                      fill="white"
+                      fillOpacity="0.7"
+                      rx="14"
+                      x="2"
+                      y="2"
+                    ></rect>
+                    <path
+                      id="S"
+                      d="M18.28,13.56c-0.06,-0.49 -0.28,-0.88 -0.67,-1.16c-0.39,-0.27 -0.9,-0.41 -1.53,-0.41c-0.44,0 -0.81,0.06 -1.13,0.2c-0.31,0.13 -0.55,0.31 -0.72,0.54c-0.17,0.22 -0.25,0.48 -0.25,0.78c0,0.24 0.05,0.45 0.16,0.63c0.11,0.17 0.27,0.33 0.46,0.45c0.19,0.12 0.41,0.22 0.64,0.31c0.24,0.08 0.47,0.15 0.71,0.21l1.09,0.27c0.44,0.1 0.87,0.24 1.27,0.42c0.41,0.17 0.77,0.39 1.1,0.65c0.32,0.27 0.58,0.59 0.77,0.96c0.19,0.38 0.28,0.82 0.28,1.32c0,0.68 -0.17,1.29 -0.52,1.8c-0.35,0.52 -0.85,0.92 -1.51,1.21c-0.66,0.29 -1.45,0.44 -2.38,0.44c-0.91,0 -1.69,-0.14 -2.36,-0.42c-0.66,-0.28 -1.18,-0.69 -1.56,-1.23c-0.37,-0.54 -0.57,-1.19 -0.6,-1.97h2.08c0.03,0.41 0.15,0.74 0.37,1.01c0.22,0.27 0.51,0.47 0.86,0.61c0.35,0.13 0.75,0.2 1.19,0.2c0.46,0 0.86,-0.07 1.21,-0.21c0.34,-0.14 0.62,-0.33 0.81,-0.58c0.2,-0.25 0.3,-0.54 0.31,-0.87c-0.01,-0.31 -0.1,-0.56 -0.27,-0.75c-0.18,-0.2 -0.42,-0.37 -0.73,-0.5c-0.31,-0.14 -0.68,-0.26 -1.1,-0.37l-1.32,-0.34c-0.96,-0.24 -1.71,-0.62 -2.27,-1.12c-0.55,-0.5 -0.83,-1.17 -0.83,-2c0,-0.69 0.18,-1.29 0.56,-1.8c0.37,-0.52 0.88,-0.92 1.52,-1.2c0.65,-0.29 1.38,-0.44 2.19,-0.44c0.83,0 1.55,0.15 2.17,0.44c0.63,0.28 1.12,0.68 1.47,1.18c0.36,0.51 0.54,1.09 0.55,1.74z"
+                      fill="currentColor"
+                      data-node-id="1:616"
+                    ></path>
+                  </g>
+                </svg>
+              </div>
+            </div>
+            <p className="savings-text">Savings</p>
+            <p className="savings-amount">$ 3,000</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ValueProposition;
