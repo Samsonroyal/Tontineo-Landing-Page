d8558d2b127b164cee821c284397619824fb5f61
Were Samson Bruno <werefast2000@gmail.com>
2024-01-21 23:52:22 +0000

    created benefits component

--- src/components/Benefits/index.jsx
+++ src/components/Benefits/index.jsx
@@ -1,42 +1,127 @@
-import React from 'react';
+import React from "react";
 
 const Benefits = () => {
-    return (
-        <div className="tontine-group-info">
-            <p className="trusted-by-tontine-groups1">
-                <span className="trusted-by-tontine-groups">Trusted by </span>
-                <span className="active-tontine-groups-in-west-africa1">10</span>
-                <span className="active-tontine-groups-in-west-africa"> </span>
-                <span className="trusted-by-tontine-groups">active </span>
-                <span>tontine groups in West Africa</span>
-            </p>
-            <div className="tontine-group-info1">
-                <div className="personal-contributions2">
-                    <div className="container-with-images">
-                        <img src="/assets/img_1_513_fa8e7e.png" className="image-container5" />
-                        <img src="/assets/img_1_514_80e9f4.png" className="image-container8" />
-                    </div>
-                </div>
-                <div className="personal-contributions-section">
-                    <div className="personal-contributions1">
-                        <p className="personal-contributions-heading">Keep track of all your personal contributions to the tontine</p>
-                        <p className="personal-contributions-text">We are building a digital community that will empower you to take control of your money and make it work for you.</p>
-                    </div>
-                    <div className="personal-contributions">
-                        <p className="learn-more">Learn More</p>
-                        <div className="svg-container2 svg-container2">
-                            <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" x="0" y="0">
-                                <g id="arrow-right" xmlns="http://www.w3.org/2000/svg" data-node-id="1:521">
-                                    <path id="Vector_44" d="M5,12h14" stroke="#068353" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" data-node-id="1:522"></path>
-                                    <path id="Vector_45" d="M12,5l7,7l-7,7" stroke="#068353" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" data-node-id="1:523"></path>
-                                </g>
-                            </svg>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="tontine-group-info">
+      <p className="trusted-by-tontine-groups1">
+        <span className="trusted-by-tontine-groups">Trusted by </span>
+        <span className="active-tontine-groups-in-west-africa1">10</span>
+        <span className="active-tontine-groups-in-west-africa"> </span>
+        <span className="trusted-by-tontine-groups">active </span>
+        <span>tontine groups in West Africa</span>
+      </p>
+      <div className="tontine-group-info1">
+        <div className="personal-contributions2">
+          <div className="container-with-images">
+            <img
+              src="/assets/img_1_513_fa8e7e.png"
+              className="image-container5"
+            />
+            <img
+              src="/assets/img_1_514_80e9f4.png"
+              className="image-container8"
+            />
+          </div>
         </div>
-    );
+        <div className="personal-contributions-section">
+          <div className="personal-contributions1">
+            <p className="personal-contributions-heading">
+              Keep track of all your personal contributions to the tontine
+            </p>
+            <p className="personal-contributions-text">
+              We are building a digital community that will empower you to take
+              control of your money and make it work for you.
+            </p>
+          </div>
+          <div className="personal-contributions">
+            <p className="learn-more">Learn More</p>
+            <div className="svg-container2 svg-container2">
+              <svg
+                fill="none"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                x="0"
+                y="0"
+              >
+                <g
+                  id="arrow-right"
+                  xmlns="http://www.w3.org/2000/svg"
+                  data-node-id="1:521"
+                >
+                  <path
+                    id="Vector_44"
+                    d="M5,12h14"
+                    stroke="#068353"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    data-node-id="1:522"
+                  ></path>
+                  <path
+                    id="Vector_45"
+                    d="M12,5l7,7l-7,7"
+                    stroke="#068353"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    data-node-id="1:523"
+                  ></path>
+                </g>
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="tontineo-section">
+        <div className="money-protection-container">
+          <p className="personal-contributions-heading">
+            Safeguard your money from being squandered
+          </p>
+          <p className="personal-contributions-text">
+            We are building a digital community that will empower you to take
+            control of your money and make it work for you.
+          </p>
+          <div className="money-protection-container1">
+            <p className="learn-more2">Learn More</p>
+            <div className="svg-container2">
+              <svg
+                fill="none"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                x="0"
+                y="0"
+              >
+                <g id="arrow-right_2">
+                  <path
+                    id="Vector_46"
+                    d="M5,12h14"
+                    stroke="#0EA730"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                  ></path>
+                  <path
+                    id="Vector_47"
+                    d="M12,5l7,7l-7,7"
+                    stroke="#0EA730"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                  ></path>
+                </g>
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div className="money-control-section">
+          <img
+            src="/assets/img_1_536_c4476b.png"
+            className="image-container20"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Benefits;
